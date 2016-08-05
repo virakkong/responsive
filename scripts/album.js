269d09cd6fbcbf04782593c353c3ee6f35faf81e
vireakyutkong@gmail.com <vireakyutkong@gmail.com>
2016-08-05 12:25:40 -0700

    assignment commpleted: 11. DOM Scripting: Album View

--- scripts/album.js
+++ scripts/album.js
@@ -51,10 +51,28 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
+//Create Third Album
 
+var myAlbum = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/02.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
 var setCurrentAlbum = function(album){
     //#1
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
+   
     var albumTitle = document.getElementsByClassName('album-view-title')[0];
     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
@@ -64,6 +82,7 @@ var setCurrentAlbum = function(album){
     //#2
     //each of the representative of the node will take albumPicasso which is represented by album
     albumImage.setAttribute('src',album.albumArtUrl);
+    resetAlbum = albumImage.setAttribute('src',album.albumArtUrl);
     albumTitle.firstChild.nodeValue= album.title;
     albumArtist.firstChild.nodeValue= album.artist;
     albumReleaseInfo.firstChild.nodeValue= album.year + ' ' + album.label;
@@ -82,10 +101,33 @@ var setCurrentAlbum = function(album){
 
 
 window.onload = function() {
+   setCurrentAlbum(myAlbum);
+//    var arr=[albumMarconi, myAlbum, albumPicasso];
+//    var i=0;
+//    albumImage.addEventListener('click', function (event){
+//       setCurrentAlbum(arr[i]);
+//        i++;
+//        if(i==arr.length){i=0;}
+//    });
     
-    //setCurrentAlbum(albumPicasso);
-    setCurrentAlbum(albumMarconi);
-   
+        
+             albumImage.addEventListener('click',function (event){
+                 
+                 switch(albumImage.getAttribute('src')){
+                         case 'assets/images/album_covers/01.png':
+                            setCurrentAlbum(myAlbum);
+                            break;
+                         case 'assets/images/album_covers/20.png':
+                            setCurrentAlbum(albumPicasso)
+                            break;
+                         case 'assets/images/album_covers/02.png':
+                            setCurrentAlbum(albumMarconi);
+                            break;
+                        default:
+                         setCurrentAlbum(myAlbum);
+                 }
+             
+             });  
 };
 
 
