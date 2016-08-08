a6164a769118cc9ae142c1ef31df9c41f5b7d81e
vireakyutkong@gmail.com <vireakyutkong@gmail.com>
2016-08-08 15:20:51 -0700

    completed checkpoint 16: Jquery: Landing Page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,40 +1,33 @@
 var pointsArray = document.getElementsByClassName('point');
-var animatePoints = function(points) {
-                 var revealPoint = function(index) {
-                     points[index].style.opacity = 1;
-                     points[index].style.transform = "scaleX(1) translateY(0)";
-                     points[index].style.msTransform = "scaleX(1) translateY(0)";
-                     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
+              		var revealPoint= function() {
+					    $(this).css({
+						opacity: 1,
+						transform: 'scaleX(1) translateY(0)'
+					   });
                  };
     
-                for(var i=0; i < points.length; i++){
-                    revealPoint(i);
-                }
+                $.each($('.point'), revealPoint);
         
  };
 
 
-window.onload = function() {
-    var sellingPoints= document.getElementsByClassName('selling-points')[0];
-    //Let's trigger the animation when a user scrolls at least 200 pixels into the .selling
-    //points element
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    
-    console.log(sellingPoints);
-     window.addEventListener('scroll', function(event){
-         //console.log(event);
-         //console.log("Current offset from the top is " + //sellingPoints.getBoundingClientRect().top + " pixels");
-         if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance ){
-             animatePoints(pointsArray);
-         }
-         
-         //// Automatically animate the points on a tall screen where scrolling can't trigger the animation
-         if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+$(window).load(function() {
+   
+     if ($(window).height() > 950) {
+         animatePoints();
      }
+
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    
+    $(window).scroll(function(event){
+       if($(window).scrollTop() >= scrollDistance){
+        
+         animatePoints();
+       }
                      
      });
- }
+});
 
 
             
